63e5fa950cb69cb2da2acf81a43ef1780214b0d4
Piyush <piyushshrivastava545@gmail.com>
2017-05-05 02:43:25 +0530

    plot marker of selected vehicle and calculate distance

--- app/assets/javascripts/lunar_map.js
+++ app/assets/javascripts/lunar_map.js
@@ -1,28 +1,16 @@
 var map;
-
-$(document).on('change', '#vehicle_id', function(){
-  $.get("/lunar_vehicle_map/".concat(this.value), function(data) {
-    $('#vehicle_lat').val(data.lat);
-    $('#vehicle_lng').val(data.long);
-     
-  })
-  .fail(function() {
-    console.log( "error" );
-  })
-});
-
-var options = { lat: 0.681400, lng: 23.460550 };
+var defaultOptions = { lat: 0.681400, lng: 23.460550, title: 'Apollo 11' };
+var RADIUS = 1738000;
 
 function initMap() {
    map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 0.681400, lng: 23.460550},
-    zoom: 2,
+    center: {lat: 0, lng: 0},
+    zoom: 1,
     streetViewControl: false,
     mapTypeControlOptions: {
       mapTypeIds: ['moon']
     }
   });
-
   var moonMapType = new google.maps.ImageMapType({
     getTileUrl: function(coord, zoom) {
       var normalizedCoord = getNormalizedCoord(coord, zoom);
@@ -37,13 +25,14 @@ function initMap() {
     tileSize: new google.maps.Size(256, 256),
     maxZoom: 9,
     minZoom: 0,
-    radius: 1738000,
+    radius: RADIUS,
     name: 'Moon'
   });
 
   map.mapTypes.set('moon', moonMapType);
   map.setMapTypeId('moon');
-  map.data.addGeoJson(coords(options));
+  setStyle();
+  map.data.addGeoJson(coords(defaultOptions));
 }
 
 function coords(options) {
@@ -52,10 +41,20 @@ function coords(options) {
     features: [{
       type: 'Feature',
       geometry: { type: 'Point', coordinates: [parseFloat(options.lat), parseFloat(options.lng)] },
-      properties: { name: 'Apollo 11' }
+      properties: { name: options.title }
     }]
   }
 }
+
+function setStyle() {
+  map.data.setStyle(function(feature) {
+    return {
+      title: feature.getProperty('name'),
+      optimized: false
+    };
+  });
+}
+
 // Normalizes the coords that tiles repeat across the x axis (horizontally)
 // like the standard Google map tiles.
 function getNormalizedCoord(coord, zoom) {
@@ -78,3 +77,33 @@ function getNormalizedCoord(coord, zoom) {
 
   return {x: x, y: y};
 }
+
+// Used Spherical Law of Cosines
+
+function calculateDistance(options) {
+  var R = RADIUS/1000; // km
+  var d = Math.acos(Math.sin(defaultOptions.lat)*Math.sin(options.lat) + 
+                  Math.cos(defaultOptions.lat)*Math.cos(options.lat) *
+                  Math.cos(options.lng-defaultOptions.lng)) * R; // in km
+      d = d * 0.6213; //in miles
+  $("#distance").text(d.toFixed(2).concat(' miles'));
+}
+
+
+$(document).on('change', '#vehicle_id', function(){
+  $.get("/lunar_vehicle_map/".concat(this.value), function(data) {
+    $('#vehicle_lat').val(data.lat);
+    $('#vehicle_lng').val(data.long);
+    $('#vehicle_name').val(data.name);
+  })
+  .fail(function() {
+    console.log( "error" );
+  })
+});
+
+$(document).on('click', '#locate', function() {
+  options = { lat: $('#vehicle_lat').val(), lng: $('#vehicle_lng').val(), title: $('#vehicle_name').val() };
+  initMap();
+  map.data.addGeoJson(coords(options));
+  calculateDistance(options)
+});
